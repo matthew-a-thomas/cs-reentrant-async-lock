2739d5b0657f7d5ad21afa153c0cfd010f245488
Matt Thomas <me@matthewathomas.com>
2022-06-21 13:16:13 -0400

    Demonstrate that Stephen Cleary's prototype `RecursiveAsyncLock` isn't 100% correct:
    ```
    Xunit.Sdk.EqualException
    Assert.Equal() Failure
    Expected: 10000
    Actual:   9868
       at RecursiveAsyncLock.Tests.ReentrantAsyncLockClass.LockAsyncMethodShould.<SerializeReentrantCode>d__2.MoveNext() in D:\GitHub\cs-reentrant-async-lock\ReentrantAsyncLock.Tests\ReentrantAsyncLockClass.cs:line 115
    --- End of stack trace from previous location where exception was thrown ---
       at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
       at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
       at Xunit.Sdk.TestInvoker`1.<>c__DisplayClass48_1.<<InvokeTestMethodAsync>b__1>d.MoveNext() in C:\Dev\xunit\xunit\src\xunit.execution\Sdk\Frameworks\Runners\TestInvoker.cs:line 264
    --- End of stack trace from previous location where exception was thrown ---
       at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
       at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
       at Xunit.Sdk.ExecutionTimer.<AggregateAsync>d__4.MoveNext() in C:\Dev\xunit\xunit\src\xunit.execution\Sdk\Frameworks\ExecutionTimer.cs:line 48
    --- End of stack trace from previous location where exception was thrown ---
       at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
       at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
       at Xunit.Sdk.ExceptionAggregator.<RunAsync>d__9.MoveNext() in C:\Dev\xunit\xunit\src\xunit.core\Sdk\ExceptionAggregator.cs:line 90
    ```

--- ReentrantAsyncLock.Tests/ReentrantAsyncLockClass.cs
+++ ReentrantAsyncLock.Tests/ReentrantAsyncLockClass.cs
@@ -1,7 +1,5 @@
 ﻿namespace ReentrantAsyncLock.Tests;
 
-using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -13,22 +11,22 @@ public class ReentrantAsyncLockClass
         [Fact]
         public async Task SupportAsynchronousReentrancy()
         {
-            var asyncLock = new ReentrantAsyncLock();
+            var asyncLock = new RecursiveAsyncLock();
             await Task.Run(async () =>
             {
-                await using (await asyncLock.LockAsync(CancellationToken.None))
+                using (await asyncLock.LockAsync(CancellationToken.None))
                 {
                     await Task.Run(async () =>
                     {
-                        await using (await asyncLock.LockAsync(CancellationToken.None))
+                        using (await asyncLock.LockAsync(CancellationToken.None))
                         {
                             await Task.Run(async () =>
                             {
-                                await using (await asyncLock.LockAsync(CancellationToken.None))
+                                using (await asyncLock.LockAsync(CancellationToken.None))
                                 {
                                     await Task.Run(async () =>
                                     {
-                                        await using (await asyncLock.LockAsync(CancellationToken.None))
+                                        using (await asyncLock.LockAsync(CancellationToken.None))
                                         {}
                                     });
                                 }
@@ -42,11 +40,11 @@ public class ReentrantAsyncLockClass
         [Fact]
         public async Task ProvideMutualExclusion()
         {
-            var asyncLock = new ReentrantAsyncLock();
+            var asyncLock = new RecursiveAsyncLock();
             var inGuardedSection = false;
             Task GenerateTask() => Task.Run(async () =>
             {
-                await using (await asyncLock.LockAsync(CancellationToken.None))
+                using (await asyncLock.LockAsync(CancellationToken.None))
                 {
                     Assert.False(inGuardedSection);
                     inGuardedSection = true;
@@ -70,14 +68,14 @@ public class ReentrantAsyncLockClass
         [Fact]
         public async Task SerializeReentrantCode()
         {
-            var asyncLock = new ReentrantAsyncLock();
+            var asyncLock = new RecursiveAsyncLock();
             var raceConditionActualValue = 0;
             var raceConditionExpectedValue = 0;
-            await using (await asyncLock.LockAsync(CancellationToken.None))
+            using (await asyncLock.LockAsync(CancellationToken.None))
             {
                 Task GenerateTask() => Task.Run(async () =>
                 {
-                    await using (await asyncLock.LockAsync(CancellationToken.None))
+                    using (await asyncLock.LockAsync(CancellationToken.None))
                     {
                         // If the code in this block is running simultaneously on multiple threads then the ++ operator
                         // is a race condition. This test detects the failure mode of that race condition--namely when
@@ -87,8 +85,8 @@ public class ReentrantAsyncLockClass
                         Interlocked.Increment(ref raceConditionExpectedValue); // This will always correctly increment, even in the face of multiple threads
                         // Another (perhaps more reliable) way to detect racing threads is to assert that the currently
                         // executing thread is the same thread that is currently processing the WorkQueue used
-                        // internally by the ReentrantAsyncLock.
-                        Assert.True(asyncLock.IsOnQueue);
+                        // internally by the RecursiveAsyncLock.
+                        // Assert.True(asyncLock.IsOnQueue);
 
                         // Asynchronously go away for a bit
                         await Task.Yield();
@@ -96,7 +94,7 @@ public class ReentrantAsyncLockClass
                         // ...then come back and do the above stuff a second time.
                         raceConditionActualValue++;
                         Interlocked.Increment(ref raceConditionExpectedValue);
-                        Assert.True(asyncLock.IsOnQueue);
+                        // Assert.True(asyncLock.IsOnQueue);
                     }
                 });
                 for (var i = 0; i < 1000; ++i)
@@ -117,74 +115,13 @@ public class ReentrantAsyncLockClass
         }
 
         [Fact]
-        public async Task LeaveQueueAfterDisposal()
-        {
-            var asyncLock = new ReentrantAsyncLock();
-            int queueThread;
-            await using(await asyncLock.LockAsync(default))
-            {
-                queueThread = Environment.CurrentManagedThreadId;
-            }
-            Assert.NotEqual(Environment.CurrentManagedThreadId, queueThread);
-            Assert.False(asyncLock.IsOnQueue);
-            Assert.IsNotType<WorkQueue>(SynchronizationContext.Current);
-        }
-
-        [Fact]
-        [SuppressMessage("ReSharper", "AccessToModifiedClosure")]
-        public async Task SupportCancellation()
-        {
-            var asyncLock = new ReentrantAsyncLock();
-            SynchronizationContext queue;
-            await using(await asyncLock.LockAsync(default))
-            {
-                queue = SynchronizationContext.Current ?? throw new Exception();
-            }
-            var mre = new ManualResetEventSlim();
-            try
-            {
-                var waiting = false;
-                queue.Post(_ =>
-                {
-                    Volatile.Write(ref waiting, true);
-                    mre.Wait();
-                }, null);
-                while (!Volatile.Read(ref waiting))
-                {}
-                // Now the queue is clogged up waiting for us to set the MRE
-                var cts = new CancellationTokenSource();
-                var mre2 = new ManualResetEventSlim();
-                var disposed = false;
-                var task = Task.Run(async () =>
-                {
-                    var lockAsync = asyncLock.LockAsync(cts.Token);
-                    mre2.Set();
-                    await using var _ = AsyncDisposable.Create(() =>
-                    {
-                        disposed = true;
-                        return default;
-                    });
-                    return await lockAsync;
-                }, CancellationToken.None);
-                mre2.Wait(CancellationToken.None);
-                cts.Cancel();
-                await Assert.ThrowsAnyAsync<OperationCanceledException>(() => task);
-                Assert.True(disposed);
-            }
-            finally
-            {
-                mre.Set();
-            }
-        }
-
-        [Fact]
         public async Task ProvideMutualExclusionOfNestedAsyncCode()
         {
-            var asyncLock = new ReentrantAsyncLock();
+            var asyncLock = new RecursiveAsyncLock();
             var raceConditionDetector = 0;
             async Task GenerateTask()
             {
-                await using (await asyncLock.LockAsync(default))
+                using (await asyncLock.LockAsync(default))
                 {
                     await Task.Run(() => ++raceConditionDetector);
                 }
@@ -200,71 +137,6 @@ public class ReentrantAsyncLockClass
                 );
             }
             Assert.Equal(5000, raceConditionDetector);
-        }
-    }
-
-    public class DocumentationShould
-    {
-        [Fact]
-        public async Task BeCorrect1()
-        {
-            var asyncLock = new ReentrantAsyncLock();
-            var raceCondition = 0;
-            // You can acquire the lock asynchronously
-            await using (await asyncLock.LockAsync(CancellationToken.None))
-            {
-                await Task.WhenAll(
-                    Task.Run(async () =>
-                    {
-                        // The lock is reentrant
-                        await using (await asyncLock.LockAsync(CancellationToken.None))
-                        {
-                            // The lock provides mutual exclusion
-                            raceCondition++;
-                        }
-                    }),
-                    Task.Run(async () =>
-                    {
-                        await using (await asyncLock.LockAsync(CancellationToken.None))
-                        {
-                            raceCondition++;
-                        }
-                    })
-                );
-            }
-            Assert.Equal(2, raceCondition);
-        }
-
-        [Fact]
-        public async Task BeCorrect2()
-        {
-            var asyncLock = new ReentrantAsyncLock();
-            await using (await asyncLock.LockAsync(CancellationToken.None))
-            {
-                await Task.Run(async () =>
-                {
-                    SynchronizationContext.SetSynchronizationContext(null);
-                    await Task.Delay(1).ConfigureAwait(false);
-                });
-                // This is fine; the lock still works
-                Assert.True(asyncLock.IsOnQueue);
-            }
-        }
-
-        [Fact]
-        public async Task BeCorrect3()
-        {
-            var asyncLock = new ReentrantAsyncLock();
-            SynchronizationContext.SetSynchronizationContext(null);
-            await Task.Yield();
-            // Now we're on the default thread pool synchronization context
-            await using (await asyncLock.LockAsync(CancellationToken.None))
-            {
-                // Now we're on a special synchronization context
-                Assert.NotNull(SynchronizationContext.Current);
-            }
-            // Now we're back on the default thread pool synchronization context
-            Assert.Null(SynchronizationContext.Current);
         }
     }
 }
---
+++ ReentrantAsyncLock.Tests/StephenCleary.cs
@@ -0,0 +1,156 @@
+﻿/*
+ * Copied with minimal changes from https://github.com/StephenCleary/AsyncEx/blob/v4/Source/Unit%20Tests/AdvancedExamples/RecursiveAsyncLockExample.cs
+ */
+#nullable disable
+namespace ReentrantAsyncLock.Tests
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.Remoting.Messaging;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Nito.AsyncEx;
+
+    // Use a logical call context to track which locks the current async logical stack frame "owns".
+        // NOTE: This approach will *only* work on .NET 4.5!
+        static class AsyncLockTracker
+        {
+            private static readonly string slotName = Guid.NewGuid().ToString("N");
+
+            private static IImmutableDictionary<RecursiveAsyncLock, Tuple<int, Task<IDisposable>>> OwnedLocks
+            {
+                get
+                {
+                    var ret = CallContext.LogicalGetData(slotName) as ImmutableDictionary<RecursiveAsyncLock, Tuple<int, Task<IDisposable>>>;
+                    if (ret == null)
+                        return ImmutableDictionary.Create<RecursiveAsyncLock, Tuple<int, Task<IDisposable>>>();
+                    return ret;
+                }
+
+                set
+                {
+                    CallContext.LogicalSetData(slotName, value);
+                }
+            }
+
+            public static bool Contains(RecursiveAsyncLock mutex)
+            {
+                return OwnedLocks.ContainsKey(mutex);
+            }
+
+            public static Task<IDisposable> Lookup(RecursiveAsyncLock mutex)
+            {
+                Tuple<int, Task<IDisposable>> value;
+                if (OwnedLocks.TryGetValue(mutex, out value))
+                    return value.Item2;
+                return null;
+            }
+
+            public static void Add(RecursiveAsyncLock mutex, Task<IDisposable> key)
+            {
+                Tuple<int, Task<IDisposable>> value;
+                if (!OwnedLocks.TryGetValue(mutex, out value))
+                    value = Tuple.Create(0, key);
+                OwnedLocks = OwnedLocks.SetItem(mutex, Tuple.Create(value.Item1 + 1, value.Item2));
+            }
+
+            public static void Remove(RecursiveAsyncLock mutex)
+            {
+                var value = OwnedLocks[mutex];
+                if (value.Item1 == 1)
+                {
+                    OwnedLocks = OwnedLocks.Remove(mutex);
+                    value.Item2.Result.Dispose();
+                }
+                else
+                {
+                    OwnedLocks = OwnedLocks.SetItem(mutex, Tuple.Create(value.Item1 - 1, value.Item2));
+                }
+            }
+        }
+
+        public sealed class RecursiveAsyncLock
+        {
+            private readonly AsyncLock mutex;
+
+            public RecursiveAsyncLock()
+            {
+                mutex = new AsyncLock();
+            }
+
+            public RecursiveAsyncLock(IAsyncWaitQueue<IDisposable> queue)
+            {
+                mutex = new AsyncLock(queue);
+            }
+
+            public int Id { get { return mutex.Id; } }
+
+            public RecursiveLockAwaitable LockAsync(CancellationToken token)
+            {
+                var key = AsyncLockTracker.Lookup(this);
+                if (key == null)
+                    key = mutex.LockAsync(token).AsTask();
+                return new RecursiveLockAwaitable(key, this);
+            }
+
+            public RecursiveLockAwaitable LockAsync()
+            {
+                return LockAsync(CancellationToken.None);
+            }
+
+            public sealed class RecursiveLockAwaitable : INotifyCompletion
+            {
+                private readonly Task<IDisposable> _key;
+                private readonly TaskAwaiter<IDisposable> _awaiter;
+                private readonly RecursiveAsyncLock _mutex;
+
+                public RecursiveLockAwaitable(Task<IDisposable> key, RecursiveAsyncLock mutex)
+                {
+                    _key = key;
+                    _awaiter = key.GetAwaiter();
+                    _mutex = mutex;
+                }
+
+                public RecursiveLockAwaitable GetAwaiter()
+                {
+                    return this;
+                }
+
+                public bool IsCompleted
+                {
+                    get { return _awaiter.IsCompleted; }
+                }
+
+                public IDisposable GetResult()
+                {
+                    var ret = _awaiter.GetResult();
+                    return new KeyDisposable(_key, _mutex);
+                }
+
+                public void OnCompleted(Action continuation)
+                {
+                    _awaiter.OnCompleted(continuation);
+                }
+
+                private sealed class KeyDisposable : IDisposable
+                {
+                    private RecursiveAsyncLock _mutex;
+
+                    public KeyDisposable(Task<IDisposable> keyTask, RecursiveAsyncLock mutex)
+                    {
+                        _mutex = mutex;
+                        AsyncLockTracker.Add(mutex, keyTask);
+                    }
+
+                    public void Dispose()
+                    {
+                        if (_mutex == null)
+                            return;
+                        AsyncLockTracker.Remove(_mutex);
+                        _mutex = null;
+                    }
+                }
+            }
+        }
+}
